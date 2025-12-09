e340a627500d428080020fcb49ff545591f4d4cb
xiyu-emma <a2004930628@gmail.com>
2025-12-09 22:21:39 +0800

    新增log mel

--- migrations/add_custom_folder_path.sql
+++
@@ -1,7 +0,0 @@
--- Migration script to add custom_folder_path column to uploads table
--- Run this if you have an existing database
-
-ALTER TABLE uploads ADD COLUMN custom_folder_path VARCHAR(255) NULL AFTER result_path;
-
--- Verify the change
-DESCRIBE uploads;
